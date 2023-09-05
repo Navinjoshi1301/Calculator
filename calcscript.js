d55a1c8b8ca776922e31c26bcb53214b2759fe6b
Navin <naveenj.iteg2022@ssism.org>
2023-09-05 15:40:20 +0530

    initial

---
+++ calcscript.js
@@ -0,0 +1,30 @@
+let string = "";
+let X="X";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'X'){
+      e="*";
+      string += e;
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == '÷'){
+      e="/";
+      string += e;
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
